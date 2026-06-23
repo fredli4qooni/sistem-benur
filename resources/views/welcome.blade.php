522f16e65d7a20586c1ecf2b2c4d7d0274229401
fredli4qooni <fredlifourqoni999@gmail.com>
2026-06-23 12:00:34 +0700

    fix(ui): use smoother SVG wave and prevent 1px gap rendering issue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -200,9 +200,9 @@
         </div>
 
         <!-- Wave Shape Divider -->
-        <div class="absolute bottom-0 left-0 w-full overflow-hidden leading-[0] z-20 pointer-events-none">
-            <svg class="relative block w-[calc(100%+1.3px)] h-[60px] md:h-[90px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V120H0V95.8C59.71,118.06,130.83,123.32,201.29,115.71C242.89,111.19,282.8,99.58,321.39,56.44Z" fill="#ffffff"></path>
+        <div class="absolute -bottom-[1px] left-0 w-full overflow-hidden leading-[0] z-20 pointer-events-none">
+            <svg class="relative block w-full h-[60px] md:h-[120px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none">
+                <path fill="#ffffff" d="M0,160L48,170.7C96,181,192,203,288,197.3C384,192,480,160,576,165.3C672,171,768,213,864,224C960,235,1056,213,1152,192C1248,171,1344,149,1392,138.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
             </svg>
         </div>
     </div>
