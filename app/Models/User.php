2656c0e1041be5f198ef00af1c0edb74035c4cd3
fredli4qooni <fredlifourqoni999@gmail.com>
2026-07-12 09:55:55 +0700

    fix(admin): allow assigning role in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password', 'status', 'phone', 'address'])]
+#[Fillable(['name', 'email', 'password', 'status', 'phone', 'address', 'role'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
